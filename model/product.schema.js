9df9c8f2d1bfee4d4d4f836fb8a0ab95ac1f2547
unknown <facubazan992009@gmail.com>
2024-01-25 21:34:11 -0300

    Estructura molde para los productos

--- model/product.schema.js
+++ model/product.schema.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose")
+
+const ProductSchema = new mongoose.Schema({
+    titulo: String,
+    precio: Number,
+    codigo: String,
+})
+
+const ProductModel = mongoose.Model("product", ProductSchema)
+module.exports = ProductModel; 
