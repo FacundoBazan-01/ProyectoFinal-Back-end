6a4a1ac53d22fa9292e07ae0823e9570ff529e0f
unknown <facubazan992009@gmail.com>
2024-01-31 19:29:15 -0300

    Validacion y funcionamiento para crear usuarios

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,9 +1,24 @@
+const UserModel = require("../model/user.schema")
+
 const getUsers =  (req, res)=>{
     res.json("Metodo Get para productos")
 }
 
-const postUsers = (req, res)=>{
-    res.json("Metodo Get para productos")
+const postUsers = async (req, res)=>{
+   try {
+    const {nombreUsuario} = req.body;
+    const userExist = await UserModel.findOne({nombreUsuario})
+    if(userExist){
+        res.status(400).json({mensaje:"Usuario ya existente"})
+        return;
+    }
+    const newUser = new UserModel(req.body)
+    await newUser.save();
+    res.status(201).json({mensaje: "Usuario creado correctamente", newUser});
+
+   } catch (error) {
+    res.status(500).json({mensaje: "Server error", error});
+   }
 }
 
 const putUsers = (req, res)=>{
