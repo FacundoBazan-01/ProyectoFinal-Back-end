ad4e54016d866619c98a18f91a42b75af09cc245
unknown <facubazan992009@gmail.com>
2024-02-02 16:47:29 -0300

    Creacion de carpetas multer y cloudinary para le manejo de imagenes

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -1,7 +1,9 @@
 import {v2 as cloudinary} from 'cloudinary';
           
 cloudinary.config({ 
-  cloud_name: 'djtdw1mi9', 
-  api_key: '744779631677255', 
-  api_secret: 'gB4WqkhMiLXRWI9yNtKuMcv_WxM' 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUD_API_KEY, 
+  api_secret: process.env.CLOUD_API_KEY 
 });
+
+module.exports = cloudinary
