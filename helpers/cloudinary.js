54f9645b2f30bd98d5906640695d03e625126138
unknown <facubazan992009@gmail.com>
2024-02-02 16:33:35 -0300

    Creacion de carpeta helpers

---
+++ helpers/cloudinary.js
@@ -0,0 +1,7 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'djtdw1mi9', 
+  api_key: '744779631677255', 
+  api_secret: 'gB4WqkhMiLXRWI9yNtKuMcv_WxM' 
+});
